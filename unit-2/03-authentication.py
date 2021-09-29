de8df0abba0f0f92096ba7d6d960c4834a96e8a8
Hayden Brown <nedyah.brown@gmail.com>
2021-09-29 12:42:23 +0000

    added authentication to unit 2

---
+++ unit-2/03-authentication.py
@@ -0,0 +1,19 @@
+# Authentication - Unit 2
+
+# Example of simple (insecure) authentication selection.
+user1 = input("Enter your username: ")
+pass1 = input("Enter your password: ")
+if user1 != "user1" and pass1 != "asD3e!": # Selection checking against username/password. Would normally be encrypted and checked from database.
+    print("Access denied.")
+else:
+    print("Access granted.")
+
+# Example of authentication selection with loop.
+user2 = input("Enter your username: ")
+pass2 = input("Enter your password: ")
+
+while user2 != "user2" and pass2 != "ffsi*K": # Essentially replaced if statement with while loop - make sure you re-run the input statements inside.
+    print("Access denied.")
+    user2 = input("Enter your username: ")
+    pass2 = input("Enter your password: ")
+print("Access granted.")
