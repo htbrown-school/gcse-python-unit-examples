a09b6d5134fe40d355a72d3460119b816c1bcae4
Hayden Brown <nedyah.brown@gmail.com>
2021-09-29 12:35:42 +0000

    added unit 2 folder

--- 01-subroutines.py
+++
@@ -1,8 +0,0 @@
-# Subroutines - Unit 2
-
-# Proceedure - no returned value
-def exampleProceedure(msg):
-    print("Example proceedure ran.")
-    print("Message:", msg)
-
-exampleProceedure("This is an example proceedure which doesn't return a value.") # Includes "msg" parameter.
