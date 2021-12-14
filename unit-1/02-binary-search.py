b666acc5da7dcee1162df58b5595e849c0b72d48
Hayden Brown <nedyah.brown@gmail.com>
2021-12-14 13:53:21 +0000

    binary search comments

--- unit-1/02-binary-search.py
+++ unit-1/02-binary-search.py
@@ -3,21 +3,21 @@
 # Using subroutines - see Unit 2: Example 01.
 
 def binarySearch(arr, searchTerm, start, end):
-    if (end < start):
+    if (end < start): # Check to see if the end value is smaller than the start variable - if it is the requested value hasn't been found.
         return False
     
-    mid = 1 + (start + end) // 2
-    if searchTerm == arr[mid]:
+    mid = 1 + (start + end) // 2 # Find the middle index.
+    if searchTerm == arr[mid]: # Is this the requested value?
         return mid
-    elif searchTerm < arr[mid]:
-        return binarySearch(arr, searchTerm, start, mid - 1)
+    elif searchTerm < arr[mid]: # If it's larger than the requested value
+        return binarySearch(arr, searchTerm, start, mid - 1) # Run the function again - form of iteration. Change variables to reflect the new array/list.
     else:
         return binarySearch(arr, searchTerm, mid + 1, end)
 
-arr = [1, 2, 3, 6, 9, 11, 14, 18, 27]
+arr = [1, 2, 3, 6, 9, 11, 14, 18, 27] # Sample array/list.
 searchTerm = int(input("Enter the value to search for: "))
 
-result = binarySearch(arr, searchTerm, 0, len(arr))
+result = binarySearch(arr, searchTerm, 0, len(arr)) # Call the function first.
 if not result:
     print("Search term not found.")
 else:
